33fb1718d88ec424f49fd43a0bf4d218da2c8082
Piyush <156369604+P202U@users.noreply.github.com>
2024-07-11 23:59:25 +0530

    Refactor

--- script.js
+++ script.js
@@ -3,51 +3,63 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
     const button = document.querySelector('button');
-
     let numSquares = 0;
 
-    const createGrid = (size) => {
+    const initializeGrid = () => {
+        numSquares = getGridSizeFromUser();
+        if (numSquares !== null) {
+            createGrid(numSquares);
+        }
+    };
 
-        container.textContent = '';
+    const getGridSizeFromUser = () => {
+        const userInput = prompt('Enter the number of squares per row/column?', 16);
+        if (userInput === null) {
+            return null;
+        }
+        const squares = parseInt(userInput);
+        if (isNaN(squares) || squares <= 0 || squares >= 100) {
+            alert('Please enter a valid number between 1 and 100.');
+            return null;
+        }
+        return squares;
+    };
+
+    const createGrid = (size) => {
+        clearContainer();
         const squareSize = 100 / size;
 
         for (let i = 0; i < size * size; i++) {
-            const div = document.createElement('div');
-            div.style.width = `${squareSize}%`;
-            div.style.height = `${squareSize}%`;
-
-            div.addEventListener('mousemove', function (e) {
-                this.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, ${Math.abs(e.offsetX - e.offsetY)})`;
-            });
-
+            const div = createSquareDiv(squareSize);
             container.appendChild(div);
         }
 
         container.style.setProperty('--num-of-divs', size);
     };
 
-    const squareDivs = () => {
-        const userInput = prompt('Enter the number of squares per row/column?', 16);
+    const createSquareDiv = (squareSize) => {
+        const div = document.createElement('div');
+        div.style.width = `${squareSize}%`;
+        div.style.height = `${squareSize}%`;
 
-        if (userInput === null) return;
+        div.addEventListener('mousemove', mouseMoveBgColor);
 
-        const squares = parseInt(userInput);
-
-        if (isNaN(squares) || squares <= 0 || squares >= 100) {
-            alert('Please enter a valid number between 1 and 100.');
-            return;
-        }
-
-        return squares;
+        return div;
     };
 
-    numSquares = squareDivs();
+    const mouseMoveBgColor = (e) => {
+        const div = e.target;
+        const { offsetX, offsetY } = e;
+        div.style.backgroundColor = `rgb(${offsetX}, ${offsetY}, ${Math.abs(offsetX - offsetY)})`;
+    };
 
-    createGrid(numSquares);
+    const clearContainer = () => {
+        container.textContent = '';
+    };
+
+    initializeGrid();
 
     button.addEventListener('click', () => {
-        numSquares = squareDivs();
-
-        createGrid(numSquares);
+        initializeGrid();
     });
 });
